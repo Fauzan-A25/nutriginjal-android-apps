b535ae53f6305c7a03d9ad61291c595ea31a4a6c
Fauzan <fauzanahsanudin@gmail.com>
2026-06-11 21:11:04 +0700

    update

--- lib/core/constants/supabase_config.dart
+++ lib/core/constants/supabase_config.dart
@@ -1,6 +1,6 @@
 class SupabaseConfig {
-  static const String url = 'https://bogged-desolate-washable.ngrok-free.dev';
-  static const String anonKey = 'eyJhbGciOiAiSFMyNTYiLCAidHlwIjogIkpXVCJ9.eyJyb2xlIjogImFub24iLCAiaXNzIjogInN1cGFiYXNlIiwgImlhdCI6IDE3NzkyNDMyMDAsICJleHAiOiAxODEwNzc5MjAwfQ.7w-1OW4iayZXq5jPNYPPmW_JXhRTqy_BticloiGqjKU';
+  static const String url = '-------';
+  static const String anonKey = '-------';
 
   // Masukkan daftar API Key Gemini Anda di sini untuk rotasi otomatis jika limit
   static const List<String> geminiApiKeys = [
